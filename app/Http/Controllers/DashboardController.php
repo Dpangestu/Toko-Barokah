7d3b3cb1fc42c2d22ae89b5ecbe9b20d3d21a3f0
Dpangestu <pangestudimas719@gmail.com>
2024-01-03 15:46:56 +0700

    update view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('layouts/main');
+        return view('pages.dashboard', [
+            'title' => 'Dashboard',
+        ]);
     }
 }
